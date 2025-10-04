aa515237b0eed18502efd1ebe42d555d400c39eb
Jamie <jamie20000303@gmail.com>
2025-10-04 23:52:19 +0900

    feature: create music collection view

---
+++ ReCoShelf/Views/MusicCollectionItemView.swift
@@ -0,0 +1,40 @@
+//
+//  MusicCollectionItemView.swift
+//  ReCoShelf
+//
+//  Created by Jamie on 2025/10/4.
+//
+
+import SwiftUI
+
+struct MusicCollectionItemView: View {
+    let title: String
+    let artist: String
+    let releaseYear: String
+    
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text(title)
+                
+                Text(artist)
+                    .font(.caption)
+                    .foregroundStyle(.secondary)
+            }
+            
+            Spacer()
+            
+            Text(releaseYear)
+                .foregroundStyle(.secondary)
+            
+            Button(action: {}) {
+                Image(systemName: "ellipsis.circle")
+            }
+        }
+        .buttonStyle(PlainButtonStyle())
+    }
+}
+
+#Preview {
+    MusicCollectionItemView(title: "Title", artist: "Artist", releaseYear: "1999")
+}
